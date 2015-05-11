85c5986a34ebaffe2caa0d1638820a9638075d84
Lena <lena_rthr@icloud.com>
2015-05-11 10:08:08 -0500

    started new chapter: lists

--- onlineEXERCISES_11_lists.py
+++ onlineEXERCISES_11_lists.py
@@ -5,11 +5,22 @@ we can also assign list values to variables and pass lists as parameters to func
 """
 import math
 
-
 def power(y):
     return math.pow(10, y)
 
+mixed_list = ["hello world", 10*2, [10, 2]]
 a = [10, 20, 30, [30, 40]]
-print(power(a[0]))
+new_list = [mixed_list, a]
+# print(power(a[0]))
+# print(mixed_list)
+# print(new_list)
 
+
+# LIST LENGTH
+
+print(len(mixed_list))
+print(len(a))
+print(len(new_list))
+# prints the uppercase of the first list item, but only the first 5 characters
+print(mixed_list[0].upper()[0:1+4])
 
