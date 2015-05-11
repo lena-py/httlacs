a130639c366a18cdeed975ec383cc1d98370061e
Lena <lena_rthr@icloud.com>
2015-05-11 15:17:50 -0500

    added examples

--- onlineEXERCISES_11_lists.py
+++ onlineEXERCISES_11_lists.py
@@ -1,5 +1,4 @@
 # LIST VALUES
-
 """
 we can also assign list values to variables and pass lists as parameters to functions.
 """
@@ -17,10 +16,19 @@ new_list = [mixed_list, a]
 
 
 # LIST LENGTH
-
 print(len(mixed_list))
 print(len(a))
 print(len(new_list))
 # prints the uppercase of the first list item, but only the first 5 characters
 print(mixed_list[0].upper()[0:1+4])
+
+
+# LIST MEMBERSHIP
+print("hello world" in mixed_list)
+print("hello" in mixed_list)
+print("hello" not in mixed_list)
+print(40 in a)
+print(2 in mixed_list)
+
+
 
