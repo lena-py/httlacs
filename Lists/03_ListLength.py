e42bab277051cd078dde16059e8b2cdd794c0a15
Lena <lena_rthr@icloud.com>
2015-06-14 14:30:12 -0500

    recoded previous exercises

---
+++ Lists/03_ListLength.py
@@ -0,0 +1,5 @@
+__author__ = 'lena'
+
+list1 = ["a", "b", [1, 2, 3, 4]]
+print(len(list1))
+print(len(list1[2]))
---
+++ Lists/04_AccessingElements.py
@@ -0,0 +1,6 @@
+__author__ = 'lena'
+
+list1 = [12, 19, 22, 47, 16]
+print(list1[2])
+print(list1[2:])
+print(list1[-1:])
